2c51a974451f8cab9acec921452e56764521512d
guillemsole <guillemsolecubilo@gmail.com>
2015-06-12 12:28:12 +0200

    Rename all the WEImageDataStandardized classes and methods that use this class

--- src/WideEyesSDKiOS/WEImageDataStandardized.m
+++ src/WideEyesSDKiOS/WEImageDataStandardized.m
@@ -1,14 +1,14 @@
 //
-//  WEImageDataStandaridized.m
+//  WEImageDataStandardized.m
 //  WideEyesSDKiOS
 //
 //  Created by Guillem  on 09/06/15.
 //  Copyright (c) 2015 WideEyes. All rights reserved.
 //
 
-#import "WEImageDataStandaridized.h"
+#import "WEImageDataStandardized.h"
 
-@implementation WEImageDataStandaridized
+@implementation WEImageDataStandardized
 
 - (instancetype)initWithContent:(NSArray *)contour andImage:(UIImage *)image {
     self = [super init];
